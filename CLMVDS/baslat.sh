3f2607d99c1e20c683eaa81d16ce796a2dae7713
carrylemore <48919856+carrylemore@users.noreply.github.com>
2022-07-12 02:58:59 +0300

    CLM VDS
    Çalmazsanız sevinirim.

---
+++ CLMVDS/baslat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HesapAdi="hesap adini yaz"
+
+while true ; do
+echo "Başlatmayı engellemek için CTRL + C yapın."
+echo "CarryLeMoreVDS 3 saniye içinde açılıyor..."
+sleep 1
+echo "CarryLeMoreVDS 2 saniye içinde açılıyor..."
+sleep 1
+echo "CarryLeMoreVDS 1 saniye içinde açılıyor..."
+sleep 1
+
+/usr/bin/mono MinecraftClient.exe hesap_adi -
+done
